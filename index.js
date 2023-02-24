932f5746d801ea8cf1f8878d99facac2bf0085a2
istiak ahmed <istiakahmed024@gmail.com>
2023-02-24 19:27:04 +0600

    mongo basic setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 require('dotenv').config()
@@ -8,6 +9,18 @@ require('dotenv').config()
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// Mongodb
+
+const uri = "mongodb+srv://cycleGuru:BOGLIBqy71Zpj8J2@cluster0.hhjg5g4.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("product-list").collection("products");
+  // perform actions on the collection object
+  // client.close();
+  console.log('db Connected');
+});
+
 
 
 app.get('/', (req, res) => {
