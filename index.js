7f5c40b0e79c44574cc968c4dc218fcf18b8b17d
istiak ahmed <istiakahmed024@gmail.com>
2023-02-27 23:58:09 +0600

    server side read me done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000
-
 require('dotenv').config()
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
@@ -13,20 +12,45 @@ app.use(express.json())
 // Mongodb
 
 const uri = "mongodb+srv://cycleGuru:BOGLIBqy71Zpj8J2@cluster0.hhjg5g4.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("product-list").collection("products");
-  // perform actions on the collection object
-  // client.close();
-  console.log('db Connected');
-});
 
+async function run() {
+  try {
 
+    const productCollection = client.db('product-list').collection('products')
+    console.log("Connected to DataBase");
+
+    // await client.connect()
+
+    // (Read) Find Multiple Documents
+    app.get('/purchase', async (req, res) => {
+      const query = {}
+      const cursor = productCollection.find(query)
+      const products = await cursor.toArray()
+      res.send(products)
+    })
+
+    // (Read) Find Single Document
+    app.get('/purchase/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) }
+      const parts = await productCollection.findOne(query)
+      res.send(parts)
+    })
+  }
+
+  finally {
+  }
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
